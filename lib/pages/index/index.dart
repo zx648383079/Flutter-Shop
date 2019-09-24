9a5ee132bcab5bcd1a28719cef7a3ebf5f2b2cd3
zodream <648383079@qq.com>
2019-09-24 17:39:30 +0800

    修改

--- lib/pages/index/index.dart
+++ lib/pages/index/index.dart
@@ -1,18 +1,30 @@
 import "package:flutter/material.dart";
-import './search_bar.dart';
+import '../../utils/index.dart';
 
-class IndexPage extends StatelessWidget {
-  const IndexPage({Key key}) : super(key: key);
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
 
+class _IndexPageState extends State<IndexPage> {
+  final String logo = getAssetUrl('/assets/images/wap_logo.png');
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: PreferredSize(
-            child: SearchBar(),
-            preferredSize: Size.fromHeight(44),
-          ),
-      ),
+        appBar: AppBar(
+          title: Text('Flutter Shop'),
+          leading: Image.network(logo),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {
+
+            },),
+            IconButton(icon: Icon(Icons.message), onPressed: () {
+
+            },)
+          ],
+        ),
+      )
     );
   }
 }
